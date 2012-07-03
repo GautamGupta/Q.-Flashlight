9850dfa56a6ac934fca5c155852352557d00ed22
Gautam Gupta <gautam@gaut.am>
2012-07-04 00:28:32 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-Q.-Flashlight
+Q. Flashlight
 =============
 
-The simplest, minimalistic, tiniest flashlight application ever!
+The simplest, minimalistic, tiniest flashlight application ever, for Android!
+
+Usage
+=====
+
+The application opens up with a black screen, just click it which would make the screen turn white and the flashlight to turn on. Clicking it again would make the screen black and the flashlight off.
+
+Features
+========
+ * Simple, minimalistic design
+ * Also enlightens up the screen with 100% brightness white color
+ * On Android 4.0 and above, you can even do other work on your mobile or lock the screen with the flashlight on
+ * No ads whatsoever
+ * Only asks for permission for the camera, nothing else
+ * Open source, licensed under GPLv3
+ * Developed by Qandro.
+
+License
+=======
+
+Q. Flashlight is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Q. Flashlight is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details [here](http://www.gnu.org/licenses/).
---
+++ src/com/qandro/flashlight/QFlashlightActivity.java
@@ -0,0 +1,146 @@
+package com.qandro.flashlight;
+
+import java.io.IOException;
+
+import com.qandro.flashlight.R;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+public class QFlashlightActivity extends Activity {
+	
+	int oriBrightnessValue;
+	Camera mCamera = null;
+	Parameters parameters;
+	LinearLayout flashControl;
+	SurfaceView preview;
+	SurfaceHolder mHolder;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		// Retrieve the brightness value for future use
+		try {
+			oriBrightnessValue = android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS);
+		} catch (SettingNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		setContentView(R.layout.main);
+		
+		flashControl = (LinearLayout) findViewById(R.id.flashcontrol);
+		preview = (SurfaceView) findViewById(R.id.preview);
+		mHolder = preview.getHolder();
+
+		flashControl.setOnClickListener(new LinearLayout.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				toggleFlashLight();
+			}});
+	}
+	
+	/**
+	 * Revert to original brightness
+	 * 
+	 * Also turn off the flashlight if api level < 14
+	 */
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		
+		// Revert to original brightness
+		setBrightness(oriBrightnessValue);
+		
+		// Turn off the flashlight if api level < 14 as leaving it on would result in a FC
+		if ( Integer.valueOf(android.os.Build.VERSION.SDK) < 14 ) {
+			turnOffFlashLight();
+		}
+	}
+	
+	/**
+	 * Toggle the flashlight on/off status
+	 */
+	public void toggleFlashLight() {
+		if (mCamera == null) { // Off, turn it on
+			turnOnFlashLight();
+		} else { // On, turn it off
+			turnOffFlashLight();
+		}
+	}
+	
+	/**
+	 * Turn on the flashlight.
+	 * 
+	 * Also set the background colour to white and brightness to max.
+	 */
+	public void turnOnFlashLight() {
+		// Safety measure if it's already on
+		turnOffFlashLight();
+		
+		// Turn on Cam
+		mCamera = Camera.open();
+		try {
+			mCamera.setPreviewDisplay(mHolder);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Turn on LED  
+		parameters = mCamera.getParameters();
+		parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		mCamera.setParameters(parameters);      
+		mCamera.startPreview();
+		
+		// Set background color
+		flashControl.setBackgroundColor(Color.WHITE);
+		
+		// Set brightness to max
+		setBrightness(100);
+	}
+	
+	/**
+	 * Turn off the flashlight if we find it to be on.
+	 * 
+	 * Also set the background to black and revert to original brightness
+	 */
+	public void turnOffFlashLight() {
+		
+		// Turn off cam
+		if (mCamera != null) {
+			mCamera.release();
+	        mCamera = null;
+		}
+		
+		// Set background color
+		flashControl.setBackgroundColor(Color.BLACK);
+		
+		// Revert to original brightness
+		setBrightness(oriBrightnessValue);
+	}
+	
+	/**
+	 * Set brightness to a desired value
+	 * @param brightness
+	 */
+	private void setBrightness(int brightness) {
+	    WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+	    layoutParams.screenBrightness = brightness/100.0f;
+	    getWindow().setAttributes(layoutParams);
+	}
+}
+
